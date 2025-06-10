c83f2c4d793e511ebc25b2eba16c1d02f78fe7af
dustin <dustinshimono@gmail.com>
2025-06-10 11:47:00 -0400

    refactor: move test fixture SQL to test/resources

--- src/main/resources/db/migration/V1__create_customers_table.sql
+++ src/main/resources/db/migration/V1__create_customers_table.sql
@@ -7,8 +7,3 @@ CREATE TABLE customers
     email_address VARCHAR(255) NOT NULL UNIQUE,
     phone_number  VARCHAR(255) NOT NULL
 );
-
-INSERT INTO customers (id, first_name, middle_name, last_name, email_address, phone_number)
-VALUES (RANDOM_UUID(), 'John', NULL, 'Smith', 'john.smith@email.com', '+1-555-123-4567'),
-       (RANDOM_UUID(), 'Maria', 'Rose', 'Garcia', 'maria.garcia@email.com', '+1-555-234-5678'),
-       (RANDOM_UUID(), 'James', NULL, 'Wilson', 'james.wilson@email.com', '+1-555-345-6789');
